c880675c9025c2a4a315f4296ae920cdedf311af
Annakrictal14 <pro100anyshka@gmail.comgit>
2024-11-08 20:51:27 +0100

    first commit

---
+++ lang.js
@@ -0,0 +1,38 @@
+const langArr = {
+   title:{
+    ua: "Мульти мовна сторінка",
+     ru: "Мультиязычная страница",
+     en: "Multi-language page",
+     de: "Mehrsprachige Seite" 
+    },
+    name :{
+        ua: "Ноутбук Lenovo Ipad",
+       ru: "Ноутбук Lenovo Ipad",
+       en: "Laptop Lenovo Ipad",
+       de: "Laptop Lenovo Ipad" 
+    },
+    os:{
+      ua: "Операційна система",
+      ru: "Операционная система",
+      en: "Operating system",
+      de: "Betriebssystem" 
+    },
+    memory: {
+    ua: "Пам'ять",
+    ru: "Память",
+    en: "Memory",
+    de: "Speicher" 
+    },
+    type :{
+        ua: "Тип пам’яті",
+        ru: "Тип памяти",
+        en: "Memory type",
+        de: "Speichertyp" 
+    },
+    color:{
+          ua: "Колір",
+        ru: "Цвет",
+        en: "Color",
+        de: "Farbe" 
+    },
+}
